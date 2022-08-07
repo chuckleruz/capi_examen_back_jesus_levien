020f0cb1b146bc5203965780532e56a03c2b987f
chuckleruz <leviensystems@gmail.com>
2022-08-06 19:08:23 -0500

    change branch "hf-edad"

---
+++ database/migrations/2022_08_06_235322_create_user_domicilio_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserDomicilioTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_domicilio', function (Blueprint $table) {
+            $table->id('user_id');
+            $table->string('domicilio');
+            $table->Integer('numero_exterior');
+            $table->string('colonia');
+            $table->Integer('cp');
+            $table->string('ciudad');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_domicilio');
+    }
+}
